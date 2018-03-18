36385a5c5c7534b3b1298a998468698061125d8e
akinorew <w.weglinska@gmail.com>
2018-03-18 19:54:41 +0100

    Unsubscribe

--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ToastService } from './toast.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-toast-component',
     templateUrl: './toast.component.html',
     styleUrls: ['./toast.component.scss']
 })
-export class ToastComponent implements OnInit {
+export class ToastComponent implements OnInit, OnDestroy {
 
     message: string;
+    private subscription: Subscription;
 
     constructor(private toastService: ToastService) { }
 
     ngOnInit() {
-        this.toastService.getAlert().subscribe((message: string) => {
+        this.subscription = this.toastService.getAlert().subscribe((message: string) => {
             this.message = message;
         });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     removeToast() {
--- src/app/wallet/wallet-list/wallet-list.component.ts
+++ src/app/wallet/wallet-list/wallet-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Record, WalletService } from '../wallet.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-wallet-list',
     templateUrl: './wallet-list.component.html',
     styleUrls: ['./wallet-list.component.scss']
 })
-export class WalletListComponent implements OnInit {
+export class WalletListComponent implements OnInit, OnDestroy {
     records: Record[];
     total: number;
+    private subscription: Subscription;
 
     constructor(private walletService: WalletService) {
     }
@@ -17,10 +19,14 @@ export class WalletListComponent implements OnInit {
         this.records = this.walletService.walletRecords;
         this.total = this.walletService.total;
 
-        this.walletService.change.subscribe(wallet => {
+        this.subscription = this.walletService.change.subscribe(wallet => {
             this.records = wallet.walletRecords;
             this.total = wallet.total;
         });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
 }
